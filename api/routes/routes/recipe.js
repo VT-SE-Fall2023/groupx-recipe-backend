ec277289240603bddd6105029e40744d0e26982c
Aniket Hirpara <gdg4dev@gmail.com>
2023-11-08 21:18:07 -0500

    added generateRecipe functionality

--- api/routes/routes/recipe.js
+++ api/routes/routes/recipe.js
@@ -3,11 +3,17 @@ const router = express.Router();
 const mongoose = require('mongoose');
 
 const recipemodel = require('../models/recipemodel');
+const getGPTResponse = require('../helpers/generateReceipe');
 
 
-router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Ready to create recipe'
+router.get('/', async (req, res, next) => {
+    const {ingredients} = req.body // parses json parameter called ingredients from the request body
+    getGPTResponse(ingredients).then(resp => {
+        // console.log(resp.choices[0].message.content)
+        res.status(200).json({
+            message: 'Ready to create recipe',
+            data: JSON.parse(resp.choices[0].message.content)
+        })
     })
 });
 
